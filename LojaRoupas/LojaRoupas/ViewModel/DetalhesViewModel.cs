317a2fe7da444c393b2693bec399d6714495f230
iagoa <iagoantunes.f@gmail.com>
2022-01-20 22:28:10 -0300

    Pagina detalhes roupas

--- LojaRoupas/LojaRoupas/ViewModel/DetalhesViewModel.cs
+++ LojaRoupas/LojaRoupas/ViewModel/DetalhesViewModel.cs
@@ -8,9 +8,13 @@ namespace LojaRoupas.ViewModel
     public class DetalhesViewModel
     {
         public string Nome { get; set; }
+        public string Imagem { get; set; }
+        public string Preco { get; set; }
         public DetalhesViewModel(Roupa roupa)
         {
             this.Nome = roupa.Nome;
+            this.Imagem = roupa.Imagem;
+            this.Preco = roupa.Preco;
         }
     }
 }
